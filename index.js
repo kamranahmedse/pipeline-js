75826649dd887187563b99a94ffd86eb5a2e1746
Kamran Ahmed <kamranahmed.se@gmail.com>
2016-09-19 19:19:08 +0500

    Add editor config and update files to match the config

--- index.js
+++ index.js
@@ -2,45 +2,45 @@
 
 function Pipeline() {
 
-    var stages = [],
-        stageOutput;
+  var stages = [],
+    stageOutput;
 
-    this.pipe = function (stage) {
-        stages.push(stage);
+  this.pipe = function (stage) {
+    stages.push(stage);
 
-        return this;
-    };
+    return this;
+  };
 
-    this.process = function (args) {
+  this.process = function (args) {
 
-        if (stages.length === 0) {
-            return args;
+    if (stages.length === 0) {
+      return args;
+    }
+
+    // Set the stageOutput to be args
+    // as there is no output to start with
+    stageOutput = args;
+
+    stages.forEach(function (stage, counter) {
+
+      // Output from the last stage was promise
+      if (typeof stageOutput.then === 'function') {
+        // Call the next stage only when the promise is fulfilled
+        stageOutput = stageOutput.then(stage);
+      } else {
+
+        // Otherwise, call the next stage with the last stage output
+        if (typeof stage === 'function') {
+          stageOutput = stage(stageOutput);
+        } else {
+          stageOutput = stage;
         }
+      }
 
-        // Set the stageOutput to be args
-        // as there is no output to start with
-        stageOutput = args;
+    });
 
-        stages.forEach(function (stage, counter) {
-
-            // Output from the last stage was promise
-            if (typeof stageOutput.then === 'function') {
-                // Call the next stage only when the promise is fulfilled
-                stageOutput = stageOutput.then(stage);
-            } else {
-
-                // Otherwise, call the next stage with the last stage output
-                if (typeof stage === 'function') {
-                    stageOutput = stage(stageOutput);
-                } else {
-                    stageOutput = stage;
-                }
-            }
-
-        });
-
-        return stageOutput;
-    };
+    return stageOutput;
+  };
 }
 
 module.exports = Pipeline;
