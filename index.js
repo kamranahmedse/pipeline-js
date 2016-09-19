2c6c2de72ed426a6558509c5f32c5ec1216060cc
Kamran Ahmed <kamranahmed.se@gmail.com>
2016-09-19 19:07:33 +0500

    Add check for when there are no stages

--- index.js
+++ index.js
@@ -7,17 +7,29 @@ function Pipeline() {
 
     this.pipe = function (stage) {
         stages.push(stage);
+
         return this;
     };
 
     this.process = function (args) {
+
+        if (stages.length === 0) {
+            return args;
+        }
+
+        // Set the stageOutput to be args
+        // as there is no output to start with
         stageOutput = args;
-        stages.forEach(function (stage, counter) {
+
+        stages.forEach(function (stage) {
+
+            //Call the next stage with the last stage output
             if (typeof stage === 'function') {
                 stageOutput = stage(stageOutput);
             } else {
                 stageOutput = stage;
             }
+
         });
 
         return stageOutput;
